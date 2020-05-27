8235685c5d614697104b45e535ef9c016f8126aa
bryan1002 <65756368+bryan1002@users.noreply.github.com>
2020-05-27 20:19:38 +0700

    Create steps.rb

---
+++ steps.rb
@@ -0,0 +1,12 @@
+@Booking
+Scenario: Booking room
+Given open the website
+Then User booking this room
+Then User closing the browser
+
+
+@contact_costumer
+Scenario : send email
+Given open the website
+Then User contact the costumer service
+Then User closing the browser
